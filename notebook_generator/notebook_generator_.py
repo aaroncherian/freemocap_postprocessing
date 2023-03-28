5ecc2963120a85c84fbfd1414f56ca8423e70bfd
aaroncherian <aaron.cherian2@gmail.com>
2023-03-28 10:24:30 -0400

    putting notebook generators into their own file

--- notebook_generator/notebook_generator_.py
+++ notebook_generator/notebook_generator_.py
@@ -1,10 +1,13 @@
 import papermill as pm
+from pathlib import Path
 
 path_to_session = r'C:\Users\aaron\FreeMocap_Data\recording_sessions\session_2023-01-17_15_47_19\15_50_24_gmt-5'
 
+path_to_this_directory = Path().resolve()/'notebook_generator'
+
 res = pm.execute_notebook(
-    'com_timeseries_plotter.ipynb',
-    'com_timeseries_plotter_output.ipynb',
+    path_to_this_directory/'timeseries_plotter.ipynb',
+    path_to_this_directory/'timeseries_plotter_output.ipynb',
     parameters = dict(path_to_session=path_to_session)
 )
 
