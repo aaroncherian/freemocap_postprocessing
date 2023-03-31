2eaa6444ca9c547eeb778911db3cd9c1a08d809a
aaroncherian <aaron.cherian2@gmail.com>
2023-03-31 14:17:01 -0400

    adding all parameters to the tree

--- freemocap_utils/postprocessing_widgets/parameter_tree_builder.py
+++ freemocap_utils/postprocessing_widgets/parameter_tree_builder.py
@@ -1,11 +1,17 @@
 
-from pyqtgraph.parametertree import ParameterTree
+from pyqtgraph.parametertree import ParameterTree, Parameter
 
-from freemocap_utils.postprocessing_widgets.parameter_widgets import interpolation_params, filter_params
+from freemocap_utils.postprocessing_widgets.parameter_widgets import interpolation_params, filter_params, rotating_params, good_frame_finder_params
+
+
+
 
 def create_main_page_parameter_tree():
     main_tree = ParameterTree()
     main_tree.addParameters(interpolation_params, showTop=False)
     main_tree.addParameters(filter_params, showTop=False)
+    main_tree.addParameters(good_frame_finder_params,showTop = False)
+    main_tree.addParameters(rotating_params, showTop=False)
+
 
     return main_tree
--- freemocap_utils/postprocessing_widgets/parameter_widgets.py
+++ freemocap_utils/postprocessing_widgets/parameter_widgets.py
@@ -1,4 +1,7 @@
-from pyqtgraph.parametertree import Parameter, ParameterTree
+from pyqtgraph.parametertree import Parameter,registerParameterType
+from PyQt6.QtWidgets import QWidget, QHBoxLayout,QVBoxLayout, QPushButton, QLabel, QLineEdit, QCheckBox
+
+
 
 interpolation_settings = [
     {"name": "Interpolation", "type": "group", "children": [
@@ -25,3 +28,85 @@ def get_interpolation_parameter(interpolation_settings):
 def get_filter_parameter(filter_settings):
     filter_params = Parameter.create(name='filter_params',type='group', children=filter_settings )
     return filter_params
+
+
+
+class CustomGoodFrameParam(Parameter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.auto_find_good_frame_param = self.child("Good Frame Finder").child('Auto-find Good Frame')
+        self.good_frame_param = self.child("Good Frame Finder").child("Good Frame")
+
+        self.auto_find_good_frame_param.sigValueChanged.connect(self.auto_find_good_frame_changed)
+
+    def auto_find_good_frame_changed(self, parameter, value):
+        if value:
+            self.good_frame_param.setValue("")
+            self.good_frame_param.setOpts(readonly=True)
+
+        else:
+            # self.good_frame_param.setValue("0")
+            self.good_frame_param.setOpts(readonly=False)
+
+
+
+registerParameterType('CustomGoodFrameParam',CustomGoodFrameParam)
+good_frame_finder_settings = [
+    {"name": "Good Frame Finder", "type": "group", "children": [
+        {"name": "Instructions", "type": "str", "value": "Uncheck 'Auto-find Good Frame' to type in the good frame manually.", "readonly": True},
+        {"name": "Auto-find Good Frame", "type": "bool", "value": True},
+        {"name": "Good Frame", "type": "str", "value": "", "step": 1},
+    ]}
+]
+good_frame_finder_params = Parameter.create(name='good_frame_finder_params', type='CustomGoodFrameParam', children=good_frame_finder_settings)
+
+
+# class RotationParameter(Parameter):
+#     def __init__(self, **opts):
+#         super(RotationParameter, self).__init__(**opts)
+#         self.addChild({'name': 'Rotate Data:', 'type': 'bool', 'value': True})
+
+#     def value(self):
+#         child_param = self.child('Rotate Data:')
+#         return child_param.value()
+
+
+rotating_settings = [
+    {"name": "Rotating", "type": "group", "children": [
+        {"name": "Rotate Data:", "type": "bool", "value": True},
+    ]}
+]
+
+rotating_params = Parameter.create(name='rotating_params', type='group', children=rotating_settings)
+
+# good_frame_finder_settings = [
+#     {
+#         "name": "Good Frame Finder",
+#         "type": "group",
+#         "children": [
+#             {"name": "Good Frame", "type": "int", "value": 0},
+#             {"name": "Auto-Find Good Frame", "type": "bool", "value":True},
+#         ],
+#     }
+# ]
+
+# good_frame_finder_params = Parameter.create(
+#     name="good_frame_finder_params", type="group", children=good_frame_finder_settings
+# )
+
+#good_frame_finder_params.child("Good Frame Finder").child('Good Frame').value()
+
+if __name__ == "__main__":
+
+    def get_all_parameter_values(parameter_object):
+        values = {}
+        for child in parameter_object.children(): #using this just to access the second level of the parameter tree
+            if child.hasChildren():
+                for grandchild in child.children():
+                    values[grandchild.name()] = grandchild.value()
+            else:
+                values[child.name()] = child.value()
+        return values
+
+    values = get_all_parameter_values(good_frame_finder_params)
+    f =2 
