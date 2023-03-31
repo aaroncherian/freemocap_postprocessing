1f109c4a24d735788d5b8e1de787b87eb877c3c7
aaroncherian <aaron.cherian2@gmail.com>
2023-03-30 21:08:49 -0400

    moving task worker to a different file

---
+++ freemocap_utils/postprocessing_widgets/task_worker_thread.py
@@ -0,0 +1,82 @@
+
+from freemocap_utils.postprocessing_widgets.parameter_widgets import interpolation_params, filter_params
+from freemocap_utils.postprocessing_widgets.postprocessing_functions.interpolate_data import interpolate_skeleton_data
+from freemocap_utils.postprocessing_widgets.postprocessing_functions.filter_data import filter_skeleton_data
+from freemocap_utils.postprocessing_widgets.postprocessing_functions.good_frame_finder import find_good_frame
+from freemocap_utils.postprocessing_widgets.postprocessing_functions.rotate_skeleton import align_skeleton_with_origin
+
+from freemocap_utils.mediapipe_skeleton_builder import mediapipe_indices
+
+from PyQt6.QtCore import QThread, pyqtSignal
+
+import numpy as np
+
+class TaskWorkerThread(QThread):
+    task_running_signal = pyqtSignal(str)
+    task_completed_signal = pyqtSignal(str, object)
+    all_tasks_finished_signal = pyqtSignal(object) 
+    def __init__(self, raw_skeleton_data:np.ndarray, task_list:list):
+        super().__init__()
+        self.good_frame = True
+        self.run_rotate_skeletons = True
+
+        self.raw_skeleton_data = raw_skeleton_data
+
+        #create a dictionary from the task list
+        self.tasks = {task_name: {'function': None, 'result': None} for task_name in task_list} 
+        # Assign the task functions here
+        self.tasks['interpolating']['function'] = self.interpolate_task
+        self.tasks['filtering']['function'] = self.filter_task
+        self.tasks['finding good frame']['function'] = self.find_good_frame_task
+        self.tasks['rotating skeleton']['function'] = self.rotate_skeleton_task
+        self.tasks['plotting']['function'] = None
+
+    def update_worker_settings(self,run_rotate_skeletons:bool, good_frame:int):
+        self.good_frame = good_frame  
+
+        if not run_rotate_skeletons:
+            self.tasks['rotating skeleton']['function'] = None
+        else:
+            self.tasks['rotating skeleton']['function'] = self.rotate_skeleton_task
+
+
+    def run(self):
+        for task_info in self.tasks.values(): #clear any previous results 
+            task_info['result'] = None
+
+        for task_name, task_info in self.tasks.items():
+            if task_info['function'] is not None:
+                self.task_running_signal.emit(task_name)
+                task_info['result'] = task_info['function']()
+                self.task_completed_signal.emit(task_name, task_info['result'])
+
+        self.all_tasks_finished_signal.emit(self.tasks)
+
+    def interpolate_task(self):
+        interpolation_values_dict = self.get_all_parameter_values(interpolation_params)
+        interpolated_skeleton = interpolate_skeleton_data(self.raw_skeleton_data, method_to_use=interpolation_values_dict['Method'])
+        return interpolated_skeleton
+
+    def filter_task(self):
+        filter_values_dict = self.get_all_parameter_values(filter_params)
+        filtered_skeleton = filter_skeleton_data(self.tasks['interpolating']['result'], order=filter_values_dict['Order'], cutoff=filter_values_dict['Cutoff Frequency'], sampling_rate=filter_values_dict['Sampling Rate'])
+        return filtered_skeleton
+
+    def find_good_frame_task(self):
+        if not self.good_frame:
+            self.good_frame = find_good_frame(self.tasks['filtering']['result'], skeleton_indices=mediapipe_indices, initial_velocity_guess=.5)
+        return self.good_frame
+
+    def rotate_skeleton_task(self):
+        origin_aligned_skeleton = align_skeleton_with_origin(self.tasks['filtering']['result'], mediapipe_indices, self.good_frame)[0]
+        return origin_aligned_skeleton
+
+    def get_all_parameter_values(self,parameter_object):
+        values = {}
+        for child in parameter_object.children(): #using this just to access the second level of the parameter tree
+            if child.hasChildren():
+                for grandchild in child.children():
+                    values[grandchild.name()] = grandchild.value()
+            else:
+                values[child.name()] = child.value()
+        return values
--- postprocess_GUI.py
+++ postprocess_GUI.py
@@ -2,26 +2,21 @@ from pathlib import Path
 import numpy as np
 
 from PyQt6.QtWidgets import QMainWindow, QWidget, QApplication, QHBoxLayout,QVBoxLayout, QPushButton, QLabel, QLineEdit, QCheckBox
-from PyQt6.QtCore import QThread, pyqtSignal
 from PyQt6.QtGui import QIntValidator
 
 from freemocap_utils.postprocessing_widgets.slider_widget import FrameCountSlider
-from freemocap_utils.postprocessing_widgets.skeleton_view_widget import SkeletonViewWidget
 from freemocap_utils.postprocessing_widgets.parameter_widgets import interpolation_params, filter_params
-from freemocap_utils.postprocessing_widgets.postprocessing_functions.interpolate_data import interpolate_skeleton_data
-from freemocap_utils.postprocessing_widgets.postprocessing_functions.filter_data import filter_skeleton_data
-from freemocap_utils.postprocessing_widgets.postprocessing_functions.good_frame_finder import find_good_frame
-from freemocap_utils.postprocessing_widgets.postprocessing_functions.rotate_skeleton import align_skeleton_with_origin
-
+from freemocap_utils.postprocessing_widgets.task_worker_thread import TaskWorkerThread
 from freemocap_utils.postprocessing_widgets.skeleton_viewers_container import SkeletonViewersContainer
-
-from freemocap_utils.mediapipe_skeleton_builder import mediapipe_indices
+from freemocap_utils.postprocessing_widgets.led_widgets import LedContainer
 
 from pyqtgraph.parametertree import ParameterTree
 
-from freemocap_utils.postprocessing_widgets.led_widgets import LEDIndicator, LedContainer
+
 
 import time
+
+
 
 class MainWindow(QMainWindow):
     def __init__(self,freemocap_raw_data:np.ndarray):
@@ -29,7 +24,8 @@ class MainWindow(QMainWindow):
         self.task_list = ['interpolating', 'filtering', 'finding good frame', 'rotating skeleton', 'plotting']
         layout = QVBoxLayout()
         widget = QWidget()
-
+        
+        self.freemocap_raw_data = freemocap_raw_data
         num_frames = freemocap_raw_data.shape[0]
         self.frame_count_slider = FrameCountSlider(num_frames)
         layout.addWidget(self.frame_count_slider)
@@ -55,7 +51,7 @@ class MainWindow(QMainWindow):
 
         self.connect_signals_to_slots()
 
-        self.skeleton_viewers_container.plot_raw_skeleton(freemocap_raw_data)
+        self.skeleton_viewers_container.plot_raw_skeleton(self.freemocap_raw_data)
 
         self.process_button = QPushButton('Process Data')
         self.process_button.clicked.connect(self.postprocess_data)
@@ -64,7 +60,6 @@ class MainWindow(QMainWindow):
     def connect_signals_to_slots(self):
         self.frame_count_slider.slider.valueChanged.connect(self.update_viewer_plots)
         
-
     def create_main_page_parameter_tree(self):
         main_tree = ParameterTree()
         main_tree.addParameters(interpolation_params, showTop=False)
@@ -86,8 +81,7 @@ class MainWindow(QMainWindow):
             
         rotate_skeleton_bool = self.rotation_check.rotation_checkbox.isChecked()
 
-        
-        self.worker_thread = WorkerThread(self.task_list)
+        self.worker_thread = TaskWorkerThread(raw_skeleton_data=self.freemocap_raw_data, task_list=self.task_list)
         self.worker_thread.update_worker_settings(run_rotate_skeletons= rotate_skeleton_bool, good_frame=self.good_frame)
         self.worker_thread.start()
         self.worker_thread.task_running_signal.connect(self.handle_task_started)
@@ -101,10 +95,7 @@ class MainWindow(QMainWindow):
         if task == 'finding good frame':
             self.good_frame_entry.good_frame_checkbox.setChecked(False)
             self.good_frame_entry.good_frame_entry.setText(str(result))
-        
-        if task in ['filtering', 'rotating skeleton']:
-           self.processed_skeleton = result
-        
+
         self.led_container.change_led_to_task_is_finished_color(task)
 
     def handle_plotting(self,task_results:dict):
@@ -118,73 +109,7 @@ class MainWindow(QMainWindow):
             self.skeleton_viewers_container.plot_processed_skeleton(self.filtered_skeleton)
 
         self.handle_task_completed('plotting')
-class WorkerThread(QThread):
-    task_running_signal = pyqtSignal(str)
-    task_completed_signal = pyqtSignal(str, object)
-    all_tasks_finished_signal = pyqtSignal(object) 
-    def __init__(self, task_list:list):
-        super().__init__()
-        self.good_frame = True
-        self.run_rotate_skeletons = True
 
-        #create a dictionary from the task list
-        self.tasks = {task_name: {'function': None, 'result': None} for task_name in task_list} 
-        # Assign the task functions here
-        self.tasks['interpolating']['function'] = self.interpolate_task
-        self.tasks['filtering']['function'] = self.filter_task
-        self.tasks['finding good frame']['function'] = self.find_good_frame_task
-        self.tasks['rotating skeleton']['function'] = self.rotate_skeleton_task
-        self.tasks['plotting']['function'] = None
-
-    def update_worker_settings(self,run_rotate_skeletons:bool, good_frame:int):
-        self.good_frame = good_frame  
-
-        if not run_rotate_skeletons:
-            self.tasks['rotating skeleton']['function'] = None
-        else:
-            self.tasks['rotating skeleton']['function'] = self.rotate_skeleton_task
-
-
-    def run(self):
-        for task_info in self.tasks.values(): #clear any previous results 
-            task_info['result'] = None
-
-        for task_name, task_info in self.tasks.items():
-            if task_info['function'] is not None:
-                self.task_running_signal.emit(task_name)
-                task_info['result'] = task_info['function']()
-                self.task_completed_signal.emit(task_name, task_info['result'])
-
-        self.all_tasks_finished_signal.emit(self.tasks)
-
-    def interpolate_task(self):
-        interpolation_values_dict = self.get_all_parameter_values(interpolation_params)
-        interpolated_skeleton = interpolate_skeleton_data(freemocap_raw_data, method_to_use=interpolation_values_dict['Method'])
-        return interpolated_skeleton
-
-    def filter_task(self):
-        filter_values_dict = self.get_all_parameter_values(filter_params)
-        filtered_skeleton = filter_skeleton_data(self.tasks['interpolating']['result'], order=filter_values_dict['Order'], cutoff=filter_values_dict['Cutoff Frequency'], sampling_rate=filter_values_dict['Sampling Rate'])
-        return filtered_skeleton
-
-    def find_good_frame_task(self):
-        if not self.good_frame:
-            self.good_frame = find_good_frame(self.tasks['filtering']['result'], skeleton_indices=mediapipe_indices, initial_velocity_guess=.5)
-        return self.good_frame
-
-    def rotate_skeleton_task(self):
-        origin_aligned_skeleton = align_skeleton_with_origin(self.tasks['filtering']['result'], mediapipe_indices, self.good_frame)[0]
-        return origin_aligned_skeleton
-
-    def get_all_parameter_values(self,parameter_object):
-        values = {}
-        for child in parameter_object.children(): #using this just to access the second level of the parameter tree
-            if child.hasChildren():
-                for grandchild in child.children():
-                    values[grandchild.name()] = grandchild.value()
-            else:
-                values[child.name()] = child.value()
-        return values
 
 class GoodFrameWidget(QWidget):
     def __init__(self):
