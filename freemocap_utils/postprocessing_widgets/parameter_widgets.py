b92d855da461dd96a1acc24e25669be9960d66aa
aaroncherian <aaron.cherian2@gmail.com>
2023-04-13 14:58:03 -0400

    Update parameter_widgets.py

--- freemocap_utils/postprocessing_widgets/parameter_widgets.py
+++ freemocap_utils/postprocessing_widgets/parameter_widgets.py
@@ -1,8 +1,6 @@
 from pyqtgraph.parametertree import Parameter,registerParameterType
 
 
-
-
 interpolation_settings = [
     {"name": "Interpolation", "type": "group", "children": [
         {
@@ -31,22 +29,12 @@ filter_settings = [
 interpolation_params = Parameter.create(name='interp_params', type='group', children=interpolation_settings)
 filter_params = Parameter.create(name='filter_params',type='group', children=filter_settings )
 
-# def get_interpolation_parameter(interpolation_settings):
-#     interpolation_params = Parameter.create(name='interp_params', type='group', children=interpolation_settings)
-#     return interpolation_params
-
-# def get_filter_parameter(filter_settings):
-#     filter_params = Parameter.create(name='filter_params',type='group', children=filter_settings )
-#     return filter_params
-
-
-
 class CustomRotationParam(Parameter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rotate_data_param = self.child("Rotation Settings").child("Rotate Data")
-        self.auto_find_good_frame_param = self.child("Rotation Settings").child('Auto-find Good Frame')
-        self.good_frame_param = self.child("Rotation Settings").child("Good Frame")
+        self.rotate_data_param = self.child("Rotation").child("Rotate Data")
+        self.auto_find_good_frame_param = self.child("Rotation").child('Auto-find Good Frame')
+        self.good_frame_param = self.child("Rotation").child("Good Frame")
 
         self.rotate_data_param.sigValueChanged.connect(self.rotate_data_changed)
         self.auto_find_good_frame_param.sigValueChanged.connect(self.auto_find_good_frame_changed)
@@ -68,7 +56,7 @@ class CustomRotationParam(Parameter):
 
 
 rotation_settings = [
-    {"name": "Rotation Settings", "type": "group", "children": [
+    {"name": "Rotation", "type": "group", "children": [
         {"name": "Rotate Data", "type": "bool", "value": True},
         {"name": "Instructions", "type": "str", "value": "Uncheck 'Auto-find Good Frame' to type in the good frame manually.", "readonly": True},
         {"name": "Auto-find Good Frame", "type": "bool", "value": True},
@@ -80,40 +68,6 @@ registerParameterType('CustomRotationParam',CustomRotationParam)
 rotation_params = Parameter.create(name='rotation_params', type='CustomRotationParam', children=rotation_settings) #the 'type' here refers to the parameter type we made the line above
 
 
-# class RotationParameter(Parameter):
-#     def __init__(self, **opts):
-#         super(RotationParameter, self).__init__(**opts)
-#         self.addChild({'name': 'Rotate Data:', 'type': 'bool', 'value': True})
-
-#     def value(self):
-#         child_param = self.child('Rotate Data:')
-#         return child_param.value()
-
-
-# rotating_settings = [
-#     {"name": "Rotating", "type": "group", "children": [
-#         {"name": "Rotate Data:", "type": "bool", "value": True},
-#     ]}
-# ]
-
-# rotating_params = Parameter.create(name='rotating_params', type='group', children=rotating_settings)
-
-# good_frame_finder_settings = [
-#     {
-#         "name": "Good Frame Finder",
-#         "type": "group",
-#         "children": [
-#             {"name": "Good Frame", "type": "int", "value": 0},
-#             {"name": "Auto-Find Good Frame", "type": "bool", "value":True},
-#         ],
-#     }
-# ]
-
-# good_frame_finder_params = Parameter.create(
-#     name="good_frame_finder_params", type="group", children=good_frame_finder_settings
-# )
-
-#good_frame_finder_params.child("Good Frame Finder").child('Good Frame').value()
 
 if __name__ == "__main__":
 
@@ -127,5 +81,5 @@ if __name__ == "__main__":
                 values[child.name()] = child.value()
         return values
 
-    values = get_all_parameter_values(good_frame_finder_params)
+    values = get_all_parameter_values(rotation_params)
     f =2 
