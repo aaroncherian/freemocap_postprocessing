e2c54b9a68a0ae00c1b92ae80627c2e453504461
aaroncherian <aaron.cherian2@gmail.com>
2023-04-17 12:31:17 -0400

    Revert "Merge pull request #4 from aaroncherian/aaron/path_input"
    This reverts commit d47854ac8411352b6b4b6ecac0ce30559c1c73e5, reversing
    changes made to dbd13a6f634b359ba34015fed03a7029db4bbca3.

--- postprocess_GUI.py
+++ postprocess_GUI.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import numpy as np
 
-from PyQt6.QtWidgets import QMainWindow, QWidget, QApplication,QVBoxLayout, QPushButton, QTabWidget,QGroupBox
+from PyQt6.QtWidgets import QMainWindow, QWidget, QApplication,QVBoxLayout, QPushButton, QTabWidget,QGroupBox, QHBoxLayout, QLineEdit, QLabel
 from PyQt6.QtGui import QPalette, QColor
-
+from PyQt6.QtCore import pyqtSignal
 
 from freemocap_utils.postprocessing_widgets.slider_widget import FrameCountSlider
 from freemocap_utils.postprocessing_widgets.task_worker_thread import TaskWorkerThread
@@ -12,7 +12,10 @@ from freemocap_utils.postprocessing_widgets.led_widgets import LedContainer
 from freemocap_utils.postprocessing_widgets.parameter_tree_builder import create_main_page_parameter_tree, create_interpolation_parameter_tree, create_filter_parameter_tree
 from freemocap_utils.postprocessing_widgets.timeseries_view_widget import TimeSeriesPlotterWidget
 from freemocap_utils.postprocessing_widgets.marker_selector_widget import MarkerSelectorWidget
+<<<<<<< HEAD
+from freemocap_utils.postprocessing_widgets.stylesheet import groupbox_stylesheet
 
+=======
 from freemocap_utils.postprocessing_widgets.stylesheet import groupbox_stylesheet, button_stylesheet
 
 
@@ -26,19 +29,23 @@ class FileManager:
         freemocap_raw_data = freemocap_raw_data[:,0:33,:]
         return freemocap_raw_data
 
-
-
+    def save_skeleton_data(self, skeleton_data, skeleton_file_name):
+        np.save(self.data_array_path/skeleton_file_name,skeleton_data)
+>>>>>>> parent of d70bb3f (Merge branch 'main' into aaron/path_input)
 
 class MainWindow(QMainWindow):
-    def __init__(self,freemocap_raw_data:np.ndarray):
+    def __init__(self,path_to_data_folder:Path):
         super().__init__()
 
+        self.file_manager = FileManager(path_to_recording=path_to_data_folder)
 
         self.resize(1256, 1029)
 
         self.setWindowTitle("Freemocap Data Post-processing")
 
         self.tab_widget = QTabWidget()
+
+        freemocap_raw_data = self.file_manager.load_skeleton_data()
 
         self.main_menu_tab = MainMenu(freemocap_raw_data=freemocap_raw_data)
         self.tab_widget.addTab(self.main_menu_tab, 'Main Menu')
@@ -51,6 +58,9 @@ class MainWindow(QMainWindow):
         self.tab_widget.addTab(self.filter_tab, 'Filtering')
         
         self.setCentralWidget(self.tab_widget)
+
+        self.main_menu_tab.save_skeleton_data_signal.connect(self.file_manager.save_skeleton_data)
+
 
         f = 2
 
@@ -176,9 +186,11 @@ class FilteringMenu(QWidget):
        
 
 class MainMenu(QWidget):
+    
+    save_skeleton_data_signal = pyqtSignal(object,object)
     def __init__(self,freemocap_raw_data:np.ndarray):
         super().__init__()
-        self.task_list = ['interpolating', 'filtering', 'finding good frame', 'rotating skeleton', 'plotting']
+        self.task_list = ['interpolation', 'filtering', 'finding good frame', 'skeleton rotation', 'results visualization', 'data saved']
         layout = QVBoxLayout()
 
         self.setStyleSheet(groupbox_stylesheet)
@@ -198,9 +210,8 @@ class MainMenu(QWidget):
 
         self.skeleton_viewers_container.plot_raw_skeleton(self.freemocap_raw_data)
 
-        self.process_button = QPushButton('Process Data')
-        self.process_button.clicked.connect(self.postprocess_data)
-        layout.addWidget(self.process_button)
+        save_groupbox = self.create_save_widgets()
+        layout.addWidget(save_groupbox)
 
         self.setLayout(layout)
 
@@ -229,6 +240,36 @@ class MainMenu(QWidget):
         groupbox.setLayout(parameter_layout)
         return groupbox
 
+    def create_save_widgets(self):
+
+        groupbox = QGroupBox('Process and save out data')
+        process_and_save_layout = QVBoxLayout()
+
+        self.process_button = QPushButton('Process data and view results')
+        self.process_button.clicked.connect(self.postprocess_data)
+        self.process_button.setStyleSheet(button_stylesheet)
+        process_and_save_layout.addWidget(self.process_button)
+
+        save_layout = QHBoxLayout()
+
+        save_label = QLabel('Name for saved file:')
+        save_layout.addWidget(save_label)
+        self.save_entry = QLineEdit()
+        self.save_entry.setMaximumWidth(600)
+        self.save_entry.setText('mediapipe_body_3d_xyz')
+        save_layout.addWidget(self.save_entry)
+
+        self.save_button = QPushButton('Save out data')
+        self.save_button.setMinimumSize(300, 20)
+        self.save_button.setStyleSheet(button_stylesheet)
+        self.save_button.clicked.connect(self.save_skeleton_data)
+        save_layout.addWidget(self.save_button)
+        save_layout.addStretch(0)
+
+        process_and_save_layout.addLayout(save_layout)
+        groupbox.setLayout(process_and_save_layout)
+        return groupbox
+
     def connect_signals_to_slots(self):
         self.frame_count_slider.slider.valueChanged.connect(lambda: self.update_viewer_plots(self.frame_count_slider.slider.value()))
         
@@ -249,14 +290,29 @@ class MainMenu(QWidget):
         self.led_container.change_led_to_task_is_running_color(task)
 
     def handle_task_completed(self,task):
-
         self.led_container.change_led_to_task_is_finished_color(task)
+
+    def save_skeleton_data(self):
+        # file_path_to_save = 
+        final_skeleton = self.get_final_processed_skeleton()
+        skeleton_save_file_name = self.save_entry.text()
+        self.save_skeleton_data_signal.emit(final_skeleton,skeleton_save_file_name)
+        self.handle_task_completed('data saved')
+
+
+    def get_final_processed_skeleton(self):
+        if self.rotated_skeleton is not None:
+            return self.rotated_skeleton
+        elif self.filtered_skeleton is not None:
+            return self.filtered_skeleton
+        else:
+            return self.interpolated_skeleton
 
     def handle_plotting(self,task_results:dict):
         good_frame = task_results['finding good frame']['result']
-        self.interpolated_skeleton = task_results['interpolating']['result']
+        self.interpolated_skeleton = task_results['interpolation']['result']
         self.filtered_skeleton = task_results['filtering']['result']
-        self.rotated_skeleton = task_results['rotating skeleton']['result']
+        self.rotated_skeleton = task_results['skeleton rotation']['result']
 
         if self.rotated_skeleton is not None:
             self.skeleton_viewers_container.plot_processed_skeleton(self.rotated_skeleton)
@@ -266,7 +322,7 @@ class MainMenu(QWidget):
         self.update_viewer_plots(good_frame)
         self.frame_count_slider.slider.setValue(good_frame)
 
-        self.handle_task_completed('plotting')
+        self.handle_task_completed('results visualization')
 
 
 if __name__ == "__main__":
@@ -278,10 +334,13 @@ if __name__ == "__main__":
     # path_to_freemocap_session_folder = Path(r'C:\Users\aaron\FreeMocap_Data\recording_sessions\recording_15_19_00_gmt-4__brit_baseline')
     # freemocap_raw_data = np.load(path_to_freemocap_session_folder/'output_data'/'raw_data'/'mediapipe3dData_numFrames_numTrackedPoints_spatialXYZ.npy')
 
-    freemocap_raw_data = np.load(path_to_freemocap_session_folder/'DataArrays'/'mediaPipeSkel_3d.npy')
-    freemocap_raw_data = freemocap_raw_data[:,0:33,:]
+    # freemocap_raw_data = np.load(path_to_freemocap_session_folder/'DataArrays'/'mediaPipeSkel_3d.npy')
+    # freemocap_raw_data = freemocap_raw_data[:,0:33,:]
+
+
+    path_to_data_folder = Path(r'D:\ValidationStudy2022\FreeMocap_Data\sesh_2022-05-24_16_10_46_JSM_T1_WalkRun')
 
     app = QApplication([])
-    win = MainWindow(freemocap_raw_data)
+    win = MainWindow(path_to_data_folder)
     win.show()
     app.exec()
